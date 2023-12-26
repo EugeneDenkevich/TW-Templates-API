97a70492463180bcb17aa2447dd0b572b55bd8e4
eugene <eugene>
2023-12-26 04:01:22 +0300

    add cors

--- src/main.py
+++ src/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.auth.base_config import auth_backend
 from src.auth.base_config import fastapi_users
 from src.auth.schemas import UserCreate
 from src.auth.schemas import UserRead
 
+
 app = FastAPI(title="Travell App")
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(
     fastapi_users.get_auth_router(auth_backend),
