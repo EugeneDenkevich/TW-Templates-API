6aae4f8824c99b375b782c8932d146ade2a0dd1e
Eugene Denkevich <eugenestudio@mail.ru>
2024-01-18 12:47:02 +0300

    create tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,2 +1,25 @@
+import httpx
+
+BASE_URL = "http://localhost:8000"
+
+
+def test_me_unauthorized():
+    res = httpx.get(
+        url=f"{BASE_URL}/users/me",
+    )
+
+    assert res.status_code == 401
+    assert res.text == '{"detail":"Unauthorized"}'
+
+
 def test_login():
-    assert 2*2==4
+    email = "string"
+    password = "string"
+
+    res = httpx.post(
+        url=f"{BASE_URL}/auth/login",
+        data={"email": email, "password": password},
+    )
+
+    assert res.status_code == 204
+    assert "ownertkn" in res.cookies
